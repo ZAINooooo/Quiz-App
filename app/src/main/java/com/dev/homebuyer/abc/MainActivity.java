2c534289201cf5a4e78c3828bde1b1c300e7e339
zain kali <zain kali>
2016-09-21 22:07:09 +0500

    single page quiz app

---
+++ app/src/main/java/com/dev/homebuyer/abc/MainActivity.java
@@ -0,0 +1,112 @@
+package com.dev.homebuyer.abc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private int currentQuestion;
+    private String[] questions;
+    private String[] answers;
+
+    private Button answerButton;
+    private Button questionButton;
+
+    private TextView questionView;
+    private TextView answerView;
+
+    private EditText answerText;
+
+
+
+
+          /*this method will run when you run your application*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+
+    public void init() {
+        questions = new String[]{"Android Applications are usually developed in which programming language", "Activity performs what on scree?"
+                , "What does ADT stands for", "What does container holds ?", "What are the key components in android archiecture"};
+        answers = new String[]{"JAVA LANGUAGE", "ACTIONS", "Android Development Tool", "OBJECTS,LABELS AND WIDGETS", "LINUX KERNEL ,LIBRARIES"};
+        currentQuestion = -1;
+        answerButton = (Button) findViewById(R.id.AnswerButton);
+        questionButton = (Button) findViewById(R.id.QuestionButton);
+
+        questionView = (TextView) findViewById(R.id.QuestionTextView);
+
+        answerView = (TextView) findViewById(R.id.AnswerTextView);
+
+        answerText = (EditText) findViewById(R.id.AnswerText);
+
+        answerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                checkAnswer();
+
+            }
+        });
+
+        questionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+
+            {
+                showQuestion();
+
+            }
+        });
+
+    }
+
+
+    private void showQuestion() {
+        currentQuestion++;
+        if (currentQuestion == questions.length)
+            currentQuestion = 0;
+        questionView.setText(questions[currentQuestion]);
+        answerView.setText("");
+    }
+
+
+    public boolean isCorrect(String answer) {
+        return (answer.equalsIgnoreCase(answers[currentQuestion]));
+    }
+
+    public void checkAnswer() {
+        String answer = answerText.getText().toString();
+
+        /*if (answer.isEmpty()) {
+            {
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(answerView);
+                toast.show();
+            }*/
+
+
+
+
+         if (isCorrect(answer))
+                answerView.setText("Right Answer!! Good Job");
+            else
+                answerView.setText("      No The Answer is :      " + "\n" + "\n" + answers[currentQuestion]);
+
+        }
+    }
+
+
